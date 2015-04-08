6bc7ebf88a2c091038f88db03c6a8197de4d3baa
zanjie <zanjie@outlook.com>
2015-04-08 10:58:30 +0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
   it { should respond_to(:password) } 
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) } 
+  it { should respond_to(:remember_token) } 
   it { should be_valid }
   describe "when name is not present" do 
     before { @user.name = " " } 
@@ -61,5 +62,9 @@ describe User do
       it { should_not eq user_for_invalid_password } 
       specify{ expect(user_for_invalid_password).to be_falsey} 
     end
+  end
+  describe "remember token" do 
+    before { @user.save } 
+    it { expect(@user.remember_token).not_to be_blank }
   end
 end
